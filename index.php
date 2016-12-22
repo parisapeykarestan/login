66cbd2b9d22ae7aeb8a1d759ec947612a48b5aee
parisapeykarestan <parisa.peykarestan@yahoo.com>
2016-12-22 18:56:25 +0330

    second commet

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
 <?php
  session_start();
 
-if (isset($_POST['txtUser']) && isset($_POST['txtPass']))
-{
-$user=$_POST['txtUser'];
-$pass=$_POST['txtPass'];
+if (isset($_POST['txtUser']) && isset($_POST['txtPass'])) {
+    $user = $_POST['txtUser'];
+    $pass = $_POST['txtPass'];
 
-try
-{
-$db=new PDO("mysql:host=localhost;dbname=user","root","");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$command = "select password from members where username=:userna";
-$stmt=$db->prepare($command);
-$stmt->bindParam(':userna', $user, PDO::PARAM_STR);
-$na=$stmt->execute();
-$hashed_password = $stmt->fetchColumn();
-if (password_verify($pass, $hashed_password))
- {
-   $_SESSION['log']="Accept";
-   
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=user", "root", "");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $command = "select password from members where username=:userna";
+        $stmt = $db->prepare($command);
+        $stmt->bindParam(':userna', $user, PDO::PARAM_STR);
+        $na = $stmt->execute();
+        $hashed_password = $stmt->fetchColumn();
+        if (password_verify($pass, $hashed_password)) {
+            $_SESSION['log'] = "Accept";
+
+        } else {
+            $_SESSION['log'] = "Failed";
+            header("Location:index.htm");
+        }
+    } catch (PDOException $err) {
+    }
 }
-else
-{
-    $_SESSION['log']="Failed";
-    header("Location:index.htm");
-}
-}
- catch(PDOException $err) {
- }
  ?>
 
 
